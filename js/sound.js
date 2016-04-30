e69f0816f50305c8ff42fc31c83b667c813183fc
Jisoo Shin <jisooxshin@gmail.com>
2016-04-30 19:20:19 -0400

    adds 3 octaves

--- js/sound.js
+++ js/sound.js
@@ -1,1 +1,32 @@
-//add code for sound
+var synth = new Tone.SimpleSynth().toMaster();
+var notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
+var oct4 = [90, 88, 67, 86, 78, 77, 188, 190];
+var oct5 = [65, 83, 68, 70, 74, 75, 76, 186];
+var oct6 = [81, 87, 69, 82, 85, 73, 79, 80];
+
+
+$(document).keydown(function(e){
+    if (e.keyCode) { 
+    	playSound(e.keyCode);
+    }
+});
+
+var playSound = function(keyCode) {
+	console.log(keyCode);
+	if(oct4.indexOf(keyCode) > -1) {
+		var idx = oct4.indexOf(keyCode);
+		var note = idx === 7 ? notes[idx] + '4' : notes[idx] + '3'
+		synth.triggerAttackRelease(note, "8n")
+	}
+	if(oct5.indexOf(keyCode) > -1) {
+		var idx = oct5.indexOf(keyCode);
+		var note = idx === 7 ? notes[idx] + '5' : notes[idx] + '4'
+		synth.triggerAttackRelease(note, "8n")
+	}
+	if(oct6.indexOf(keyCode) > -1) {
+		var idx = oct6.indexOf(keyCode);
+		var note = idx === 7 ? notes[idx] + '6' : notes[idx] + '5'
+		synth.triggerAttackRelease(note, "8n")
+	}
+};
+
